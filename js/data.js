41a40d1696f1d0c2be8b9143abbb7b61bf9b3604
Valeria Mansueto <valeria.mansueto@naranjax.com>
2026-05-01 14:26:53 -0300

    feat: agrego info en el data.js correspondiente a integrante nro 2

--- js/data.js
+++ js/data.js
@@ -29,15 +29,15 @@ window.APP_DATA = {
         },
         {
             id: '2',
-            name: 'Integrante 2',
-            location: 'Ciudad de residencia',
-            age: 20,
-            photo: '',
+            name: 'Valeria',
+            location: 'Córdoba, Provincia de Córdoba',
+            age: 30,
+            photo: '../img/perfil_vale.png',
             accent: '#c4513b',
-            skills: ['Maquetación', 'Accesibilidad', 'Diseño visual', 'Git'],
-            movies: ['Película 1', 'Película 2', 'Película 3'],
-            albums: ['Disco 1', 'Disco 2', 'Disco 3'],
-            extra: 'Disfruta combinar organización visual con detalles prácticos que hagan más agradable navegar el sitio.'
+            skills: ['Pensamiento analítico', 'Orientación a solución', 'Backend'],
+            movies: ['El diablo viste a la moda', 'El Club de la Pelea', 'Legalmente Rubia'],
+            albums: ['LUX', 'Almendra', 'La vida era mas corta'],
+            extra: 'Disfruta salir a patinar, aprender cosas nuevas y compartir experiencias con amigos.'
         },
         {
             id: '3',
@@ -94,9 +94,9 @@ window.APP_DATA = {
         {
             date: '25 de abril de 2026',
             title: 'Diseño responsivo',
-            decisions: 'Se agregaron propiedades para que la pagina pueda ser visualizada en los distintos dispositivos.',
-            challenges: 'Los elementos no se encontraban correctamente estructirados en las distintas dimensiones.',
-            changes: 'Se incorporaron las propiedades para las imagenes y organizacion de los elementos en la pagina    .'
+            decisions: 'Se agregaron propiedades para que la página pueda ser visualizada en los distintos dispositivos.',
+            challenges: 'Los elementos no se encontraban correctamente estructurados en las distintas dimensiones.',
+            changes: 'Se incorporaron las propiedades para las imágenes y organización de los elementos en la página.'
         },
         {
             date: '28 de abril de 2026',
@@ -104,6 +104,13 @@ window.APP_DATA = {
             decisions: 'Se separaron los datos del renderizado para que el sitio pueda crecer sin copiar bloques de HTML.',
             challenges: 'Había textos mal codificados y duplicación entre páginas.',
             changes: 'Se incorporaron js/data.js y js/app.js como base común para la portada, integrantes y bitácora.'
+        },
+        {
+            date: '1 de mayo de 2026',
+            title: 'Conexión HTML–JavaScript',
+            decisions: 'Se decidió eliminar el contenido hardcodeado de los archivos HTML y conectarlos al sistema de datos centralizado.',
+            challenges: 'Los HTML tenían contenido estático desincronizado con data.js, lo que obligaba a actualizar varios archivos ante cada cambio.',
+            changes: 'Se añadieron los atributos data-page y data-member-id en cada <body>, el contenedor <div id="app"></div> y las etiquetas <script> que cargan data.js y app.js. Ahora todo el contenido lo genera app.js a partir de los datos.'
         }
     ]
 };
